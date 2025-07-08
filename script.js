334a8aa9c17f4cd548d5a45c5851f913c7e0b118
Aniket Gupte <aniketgupte11@gmail.com>
2025-07-08 17:12:05 +0530

    Refactor index.html structure and add scriptguess.js for message logging

--- script.js
+++ script.js
@@ -73,3 +73,28 @@ const calcTempAmplitude = function (temps) {
 };
 const amplitude = calcTempAmplitude(finalTemperatures);
 console.log(amplitude);
+
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+
+    // C) FIX
+    value: Number(prompt("Degrees celsius:")),
+    //value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
